cf40a954fe47a87d639c379aae68b47f0449cebf
anabana21 <157493260+anabana21@users.noreply.github.com>
2024-07-25 17:48:32 +0200

    added new components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./logo.png";
 import './App.css';
+import Dictionary from "./Dictionary";
 
  function App() {
   return (
@@ -7,6 +8,12 @@ import './App.css';
       <header className="App-header">
         <img src={logo} className="App-logo img-fluid" alt="logo" />
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      <footer className="App-Footer">
+        Coded by Ana Vogt
+      </footer>
     </div>
   );
 }
